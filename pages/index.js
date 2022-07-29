a87fe69baef4a0f08f483350a0919c14238a12e6
C <45455218+crypblizz8@users.noreply.github.com>
2022-07-29 13:25:48 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,6 @@ export default function Home() {
   }, [address]);
 
   // Set background to user's favorite color.
-  // Test Comment for the Vercel deploy
   useEffect(() => {
     if (!favoriteColor) return;
     document.body.style = `background: ${favoriteColor};`;
